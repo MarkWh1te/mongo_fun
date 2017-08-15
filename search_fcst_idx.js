6dc4679663d245941ea093c7d89813f0c1cd70b0
unknown <tianjun.ma@gticom.cn>
2017-08-15 16:17:15 +0800

    upldate
    d

--- search_fcst_idx.js
+++ search_fcst_idx.js
@@ -1,15 +1,23 @@
 var i = 1 
+
 var fields_list = ['trade_date','symbol','mkt_idx.tcap'];
+var fields_dict = fields_list.map(x=>({x:{'$exist':false}}))
 var search_dict = {
         'trade_date':{
          '$gte':ISODate('20070101'),
          '$lte':ISODate('20170728')
-    }
+    },
+        'fsct_idx.es_3year':{
+            'exist':true
+        }
 }
 db.getCollection('Z3_EQUITY_HISTORY').find(
     search_dict,
     {}
-).sort({'trade_date':i})
+).sort({'trade_date':i}).count()
+
+
+
 // db.getCollection('Z3_EQUITY_HISTORY').find(
 // {
 //     // 'symbol':/^3/,
